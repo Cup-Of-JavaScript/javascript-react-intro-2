e6e27400ad01ff7993a55fcb21087460fe8fbcb4
steven davis <sdavis@xpanxion.com>
2022-06-20 11:12:06 -0700

    update

---
+++ src/Student9/Ex1/DateFetcher.js
@@ -0,0 +1,17 @@
+import React, { useState}  from 'react'
+
+export const DateFetcher = () => {
+    const [date, setDate] = useState(0)
+
+    const onHandleClick = () => {
+        let time = new Date().toUTCString();
+        setDate(time)
+    }
+
+  return (
+    <div>
+     <button onClick={()=>onHandleClick()}>Push</button>UTC Date: {date}
+    </div>
+  )
+}
+   
--- src/Student9/Ex1/Main.js
+++ src/Student9/Ex1/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import './Main.css'
+import { DateFetcher } from './DateFetcher'
+import './Main.scss'
 
 export const Main = () => {
   return (
     <div>
-      TODO...
+      <DateFetcher />
     </div>
   )
 }
