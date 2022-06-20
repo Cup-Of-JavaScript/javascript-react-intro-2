8a06d7438554d16bcc2f2319c97d00738a07bb17
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-20 11:14:02 -0700

    indentation

--- src/Student7/Ex1/DateFetcher.js
+++ src/Student7/Ex1/DateFetcher.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const DateFetcher = () => {
-  const [time, setTime] = useState('Unknown')
-
-
+  const [time, setTime] = useState("Unknown");
   const onHandleClick = () => {
-      setTime(new Date().toUTCString())
-    }
-
-
+    setTime(new Date().toUTCString());
+  };
   return (
     <div>
-    <button onClick={() => onHandleClick()}>Push</button>UTC Date: {time} 
+      <button onClick={() => onHandleClick()}>Push</button>UTC Date: {time}
     </div>
-  )
-}
+  );
+};
