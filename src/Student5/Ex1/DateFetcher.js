26dd40ea982e4f9dddcc9d0c475364537176fc68
Diane Tewah <dtewah@xpanxion.com>
2022-06-20 14:12:43 -0400

    Ex. 1: Date Fetcher

--- src/Student5/Ex1/DateFetcher.js
+++ src/Student5/Ex1/DateFetcher.js
@@ -2,17 +2,12 @@ import React from "react";
 import { useState } from "react";
 
 export const DateFetcher = () => {
- 
-
-  const [date, setDate] = useState('unknown');
+  const [date, setDate] = useState("unknown");
 
   const onHandleClick = () => {
-  let date = new Date().toUTCString()
+    let date = new Date().toUTCString();
     setDate(date);
-  
- 
-
-};
+  };
 
   return (
     <div>
