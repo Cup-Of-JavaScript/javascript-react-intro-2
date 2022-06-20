09856de00ca81fbea0a04707bf07fa64d33ed6ca
steven davis <sdavis@xpanxion.com>
2022-06-20 14:32:48 -0700

    update

---
+++ src/Student9/Ex2/TenCounter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+
+export const TenCounter = () => {
+    let [counter, setCounter] = useState(0);
+
+    const onHandleClick = () => {
+        if (counter === 10){
+            setCounter(0);
+        }
+        else {
+            setCounter(++counter);
+        }
+        
+    }
+
+  return (
+    <div>
+        <button onClick={() => onHandleClick()}>Push</button>
+        Counter: {counter}
+    </div>
+  )
+}
+   
