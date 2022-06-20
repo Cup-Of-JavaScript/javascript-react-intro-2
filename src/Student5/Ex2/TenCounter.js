a91dbe53075f1d8373468bbac6a7f1065cbc19b3
Diane Tewah <dtewah@xpanxion.com>
2022-06-20 14:41:04 -0400

    Ex. 2: TenCounter

---
+++ src/Student5/Ex2/TenCounter.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+
+export const TenCounter = () => {
+  let [counter, setCounter] = useState(0);
+
+  const onClick = () => {
+    if (counter <= 9) {
+      setCounter(++counter);
+    } else setCounter(0);
+  };
+
+  return (
+    <div>
+      <button onClick={() => onClick()}>Push</button> Counter: {counter}
+    </div>
+  );
+};
