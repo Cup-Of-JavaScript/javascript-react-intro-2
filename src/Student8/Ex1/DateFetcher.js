bbb1d898f9626f2624355853b97028209e7eceec
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-20 12:22:32 -0400

    Ex1 Date Fetcher

---
+++ src/Student8/Ex1/DateFetcher.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+export const DateFetcher = () => {
+    let [mdate, getDate] = useState(null)
+
+    const onHandleDate = () => {
+        let mdate = new Date().toUTCString();
+        getDate(mdate);
+    }
+
+  return (<div>
+    <button onClick={() => onHandleDate()}>Push</button>
+    UTC Date: {mdate}
+    </div>
+  )
+}
