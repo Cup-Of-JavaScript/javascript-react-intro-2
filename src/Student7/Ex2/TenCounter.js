5d0f9f5e6acea27728de58dd2d9f5441ef3676ca
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-20 12:39:58 -0700

    colution w/ clean code

--- src/Student7/Ex2/TenCounter.js
+++ src/Student7/Ex2/TenCounter.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
 
 export const TenCounter = () => {
-    let [increase, setIncrease] = useState(0);
-    const onHandleClick = (count) => {
-        if (count === 'count' ){
+  let [increase, setIncrease] = useState(0);
+  const onHandleClick = () => {
+    if (increase < 10) {
       setIncrease(++increase);
-        } else {
-            setIncrease(--increase);
-        }
-    };
+    } else {
+      setIncrease(0);
+    }
+  };
   return (
     <div>
-        <button onClick={() => onHandleClick('count')}>Push</button>Counter: {increase}
+      <button onClick={() => onHandleClick()}>Push</button>Counter:
+      {increase}
     </div>
-  )
-}
+  );
+};
