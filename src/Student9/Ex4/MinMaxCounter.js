4f4adae0a35d9cf1c0d38dd96d7461dc47c6b022
steven davis <sdavis@xpanxion.com>
2022-06-22 07:35:53 -0700

    update

--- src/Student9/Ex4/MinMaxCounter.js
+++ src/Student9/Ex4/MinMaxCounter.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export const MinMaxCounter = () => {
-    let [count, setCount] = useState(0);
-    let [Min,setMin] = useState(0)
-    let[max,setMax] = useState(0)
-    let increase = () => {
-        setCount(count + 1);
+  let [count, setCount] = useState(0);
+  let [Min, setMin] = useState(0);
+  let [Max, setMax] = useState(0);
+  let increase = () => {
+    setCount(++count);
+    if (count > Max) {
+      setMax((Max = count));
     }
+  };
 
-    let decrease = () => {
-        setCount(count - 1)
+  let decrease = () => {
+    setCount(--count);
+    if (count < Min) {
+      setMin((Min = count));
     }
-    return(
-        <div>
-        <button onClick={decrease}>Down</button>
-        {count}
-        <button onClick={increase}>Up</button>
-        <br/>
-        Min:{minimum}
-        <br/>
-        Max:{maxium}
+  };
+  return (
+    <div>
+      <button onClick={decrease}>Down</button>
+      {count}
+      <button onClick={increase}>Up</button>
+      <br />
+      Min:{Min}
+      <br />
+      Max:{Max}
     </div>
-    )
-  
-}
+  );
+};
