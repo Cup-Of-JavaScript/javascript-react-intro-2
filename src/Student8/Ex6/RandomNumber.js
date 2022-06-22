c487160a1305528ded9bf93448d66c1f43f10e6e
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-22 14:47:28 -0400

    Ex 6 Made Random an arrow function

--- src/Student8/Ex6/RandomNumber.js
+++ src/Student8/Ex6/RandomNumber.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const RandomNumber = () => {
     const [num, setNum] = useState(0);
 
-    function randomNumberInRange(min, max) {
+   const randomNumberInRange = (min, max) => {
       // 👇️ get number between min (inclusive) and max (inclusive)
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
