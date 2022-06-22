e20942635d4984fbac88874e0a51997d1c40238f
Antionette Van <avan@xpanxion.com>
2022-06-22 09:01:42 -0700

    update

---
+++ src/Student3/Ex2/TenCounter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const TenCounter = () => {
+    let [counter, setCounter] = useState(0)
+    const onHandleClick = () => {
+        if (counter >= 10) { setCounter(0) }
+        else { setCounter(++counter) }
+    }
+
+    return (
+        <div className='TenCounter'>
+            <button onClick={() => onHandleClick()}>Push</button>
+            Counter: {counter}
+        </div>
+    )
+}   
