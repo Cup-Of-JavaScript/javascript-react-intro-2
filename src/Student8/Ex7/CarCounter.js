6603e5bf13737e6f5d79372d091ebc0eb721d2ed
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-22 15:24:36 -0400

    Ex 7 Car Counter

---
+++ src/Student8/Ex7/CarCounter.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const CarCounter = () => {
+
+    let [ford, setFord] = useState(0);
+    let [toyota, setToyota] = useState(0);
+    let [chevy, setChevy] = useState(0);
+    let total = ford + toyota + chevy
+
+    const onHandleClickFord = () => {
+        setFord(++ford);
+    }
+    const onHandleClickToyota = () => {
+        setToyota(++toyota);
+    }
+    const onHandleClickChevy = () => {
+        setChevy(++chevy);
+    }
+
+
+    return (
+        <div>
+            <button onClick={() => onHandleClickFord()}>Ford</button>
+            <button onClick={() => onHandleClickToyota()}>Toyota</button>
+            <button onClick={() => onHandleClickChevy()}>Chevy</button>
+            <br />
+            Ford: {ford}<br />
+            Toyota: {toyota}<br />
+            Chevy: {chevy}<br />
+            TOTAL:  {total}
+
+
+
+
+        </div>
+    )
+}
+
+export default CarCounter
