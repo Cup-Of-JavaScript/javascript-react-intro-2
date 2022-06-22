31f7ff31f4b3ca05d5298daac6e6c014627a2326
Sherika Fayson <sfayson@xpanxion.com>
2022-06-22 15:04:20 -0400

    Ex5: NumberBuilder

---
+++ src/Student10/Ex5/NumberBuilder.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './NumberBuilder.scss'
+import { useState } from 'react'
+
+export const NumberBuilder = () => {
+    let [counter, setCounter] = useState(0);
+    const [numbers, setNumbers] = useState([])
+
+    const onHandleClick = () => {
+       setCounter(++counter);
+        numbers.push(counter)
+    }
+
+    const onReset = () => {
+        setNumbers([]);
+        setCounter(0)
+    }
+
+  return (
+    <div>
+        <button onClick={() => onHandleClick()}>Add Number</button>
+        <button onClick={onReset}>Reset</button>
+        <br/>
+        Numbers: {numbers.join("")}
+        
+        
+    </div>
+  )
+}
