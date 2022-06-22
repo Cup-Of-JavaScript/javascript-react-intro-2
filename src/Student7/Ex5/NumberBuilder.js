8da143889a83d5e6d3c1319608d5ab138f193ca7
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-22 13:10:50 -0700

    adding event handlers

--- src/Student7/Ex5/NumberBuilder.js
+++ src/Student7/Ex5/NumberBuilder.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useState } from "react";
+
 
 export const NumberBuilder = () => {
+  let [count, setCount] = useState(0);
+  
+  let [numbers, setNumbers] = useState([])
+
+  const onHandleClick = (command) => {
+    if(command === 'add'){
+      setCount(++count);
+      numbers.push(count)
+    }
+
+  };
+
+  const onHandleClick2 = (command) => {
+    if(command === 'reset'){
+      setCount('');
+      setNumbers([]);
+    }
+  };
+
+
+
   return (
-    <div>NumberBuilder</div>
+    <div>
+      <button onClick={() => onHandleClick('add')}>Add Number</button>
+      <button onClick={() => onHandleClick2('reset')}>Reset</button>
+      <br />
+      Numbers: {count}
+    </div>
   )
 }
