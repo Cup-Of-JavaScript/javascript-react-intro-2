eefdc1cf98fc96f7c03b22f906bb2e2e24de334e
Antionette Van <avan@xpanxion.com>
2022-06-23 15:29:41 -0700

    update

---
+++ src/Student3/Ex4/MinMaxCounter.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { useState } from "react"
+
+export const MinMaxCounter = () => {
+    let [counter, setCounter] = useState(0);
+    let [min, setMin] = useState(0);
+    let [max, setMax] = useState(0);
+
+    const onHandleClick = (direction) => {
+        if (direction === "up") {
+            setCounter(++counter);
+            if (counter >= max) {
+                setMax(counter);
+            }
+            if
+                (direction === "down")
+                setCounter(--counter)
+            if (counter <= min) {
+                setMin(counter);
+            }
+        }
+    }
+
+    return (
+        <div className="Main">
+            <button onClick={() => onHandleClick("down")}> Down </button>
+            <button onClick={() => onHandleClick("up")}> Up </button>
+            <br /> Min: {min}
+            <br /> Max: {max}
+        </div>
+    )
+}
